a54f581de9865e80c589076abcfa740cbc649ea9
JunFe <ogum0405@hanmail.net>
2021-01-13 17:51:54 +0900

    20210113

---
+++ JavaPractice/src/sist/com/obj/core1/A.java
@@ -0,0 +1,9 @@
+package sist.com.obj.core1;
+
+import java.awt.Button;
+
+public class A extends Button {
+	public void action() {
+		System.out.println("action");
+	}
+}
---
+++ JavaPractice/src/sist/com/obj/core1/B.java
@@ -0,0 +1,8 @@
+package sist.com.obj.core1;
+
+public class B {
+	//A a;
+	public B() {
+		System.out.println("B");
+	}
+}
